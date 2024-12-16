fdab7ba2443064daf895980f69bb6e0fa8056639
Federico Mura <federicomura98@gmail.com>
2024-12-16 21:17:57 +0100

    struct clean up

--- Structs/main.go
+++ Structs/main.go
@@ -7,12 +7,15 @@ type person struct {
 	lastName  string
 }
 
+type iShuffle interface {
+}
+
 func (p person) print() {
 	fmt.Printf("%+v\n", p)
 }
 
 func (p *person) updateName(newName string) {
-	(*p).fristName = newName // do we need to *p?
+	p.fristName = newName // do we need to deference p? - no
 }
 
 func main() {
